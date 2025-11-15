f46a6c745f2a755a3c4a6a3733b54ffd5878cbc6
murtazanawroozi <murtazanawroozi812@gmail.com>
2025-11-15 02:55:17 -0800

    Add files via upload

---
+++ java basice exercise question.java
@@ -0,0 +1,87 @@
+
+package java_basice_exercise_question;
+import java.util.Scanner;
+
+public class Java_Basice_Exercise_Question {
+
+    
+    public static void main(String[] args) {
+        
+        //Circle Area
+        
+        
+        /*
+        /// Murtaza Nowroozi:
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter th eradius of circle:");
+        double radius = input.nextDouble();
+        double area = 3.14159*(radius*radius);
+        System.out.println("The erea of circle for this radius is = " + area);
+       */
+                
+                //perimeter of Rectangle:
+                /*
+        int width = 5;
+        int length  = 6;
+        int Perimeter = 2*(width + length);
+        System.out.println("The primeter of this Rectangle is " + Perimeter);
+*/
+        
+   
+        
+        // Simple interest:
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the principle:");
+        double principle = input.nextDouble();
+        
+        System.out.println("Enter the rate:");
+        double rate = input.nextDouble();
+        
+        System.out.println("Enter the year:");
+        double time = input.nextDouble();
+        double interest = (principle*rate*time)/100;
+        System.out.println("your interest after " + time + " year is " + interest);
+        double total = (interest + principle);   
+        System.out.println("The total principle is " + total);
+*/
+        
+        
+        // Evem or Odd number:
+        /*
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number:");
+        double number = input.nextDouble();
+        if(number % 2 == 0){
+            System.out.println("The number is Even.");
+        }else{
+            System.out.println("The number is Odd.");
+        }
+        */
+        
+        
+        /*
+        // Maximame of two number
+        
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first number:");
+        int a = input.nextInt();
+        System.out.println("Enter the second number:");
+        int b = input.nextInt();
+        if(a>b){
+            System.out.println("The a is grather.");
+        }if(b>a){
+            System.out.println("The b is grather.");
+        }if(a==b){
+            System.out.println("Both are equale.");}
+        }        
+        */
+        
+                   
+    }
+       
+    }    
+
+            
+    
